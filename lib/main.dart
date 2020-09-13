c6ad5362fe78a49e4a132a4c763f0bdb15c70146
Manish Nakar <manish.nakar@gmail.com>
2020-09-14 03:52:35 +0530

    scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,24 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-      home: Text("Hellow world"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+    body: Center(
+      child: Image(
+        image: AssetImage('images/diamond.png'),
+        fit: BoxFit.cover,
+      ),
+    ),
+      )
+
     );
   }
 }
